92ad5a658052a2b7dd8576722ae8a88c74f973fc
John Liu <stoic.llama@gmail.com>
2021-09-23 17:26:52 -0400

    adding admin navigation

--- App.js
+++ App.js
@@ -1,43 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 //imported for React Navigation
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 //imported for native stack
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import CategoryPicker from './CategoryPicker'
+import CategoryPicker from "./CategoryPicker";
+import AdminScreen from "./screens/AdminScreen";
 
-
-
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <CategoryPicker />
-      <StatusBar style="auto"/>
+      {/* <CategoryPicker /> */}
+      {/* <StatusBar style="auto" /> */}
+
+      <TouchableOpacity onPress={() => navigation.push("AdminScreen")}>
+        <Text>Admin</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+//////////////////////////////////////////////////
+
+// const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AdminScreen" component={AdminScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/AdminScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AdminScreen = function AdminScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default AdminScreen;
