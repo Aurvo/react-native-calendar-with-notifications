0f7db54ac23e18ec005645470e4478a174cb8bb8
unknown <dan@sdcsol.com>
2021-10-11 00:40:17 -0400

    made user categories fully synchronized with the db (both ways)

--- components/CategoryPicker.js
+++ components/CategoryPicker.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import SelectMultiple from 'react-native-select-multiple';
 import { View } from 'react-native';
-import { db } from '../firebaseconfig';
+import { db, auth } from '../firebaseconfig';
 
 // const userCategories = ['Donor', 'Volunteer', 'Client','Host'];
 
 class CategoryPicker extends Component {
     state = {
       categories: [],
-      selectedCategoryIds: []
+      selectedCategories: []
     }
     
     componentDidMount() {
@@ -18,24 +18,55 @@ class CategoryPicker extends Component {
           const data = doc.data()
           categories.push({label: data.name, value: data.id})
         });
-        this.setState({categories});
+        const uid = auth.currentUser.uid;
+        console.log('uid', uid);
+        const selectedCategories = [];
+        db.collection("user_categories").where('uid', '==', uid).get().then(uCatsSnapshot => {
+          uCatsSnapshot.forEach(doc => {
+            const data = doc.data();
+            console.log('ucat data', data);
+            selectedCategories.push(categories.find(c => c.value === data.category_id));
+          });
+          console.log('selectedCategories', selectedCategories);
+          this.setState({categories, selectedCategories});
+        });
       });
-      // const selectedCategoryIds = [];
-      // catsSnapshot.forEach(doc => selectedCategoryIds.push(doc.data().category_id));
     }
   
-    onSelectionsChange = (selectedCategories) => {
-      this.setState({selectedCategories})
+    onSelectionsChange = (selectedCategories, selectedCategory) => {
+      if (selectedCategories.length > this.state.selectedCategories.length) {
+        db.collection("user_categories").add({
+          category_id: selectedCategory.value,
+          uid: auth.currentUser.uid,
+        }).then(() => {
+          this.setState({selectedCategories});
+      })
+      } else {
+        db.collection("user_categories")
+          .where('uid', '==', auth.currentUser.uid)
+          .where('category_id', '==', selectedCategory.value)
+          .get().then(uCatSnap => {
+            // Once we get the results, begin a batch
+            const batch = db.batch();
+            uCatSnap.forEach(function(doc) {
+                // For each doc, add a delete operation to the batch
+                batch.delete(doc.ref);
+            });
+            // Commit the batch
+            batch.commit();
+            this.setState({selectedCategories});
+          });
+      }
     }
   
     render () {
-      const {categories, selectedCategoryIds} = this.state;
+      const {categories, selectedCategories} = this.state;
       console.log('categories', categories);
       return (
         <View>
           <SelectMultiple
             items={categories}
-            selectedItems={selectedCategoryIds}
+            selectedItems={selectedCategories}
             onSelectionsChange={this.onSelectionsChange}
           />
         </View>
