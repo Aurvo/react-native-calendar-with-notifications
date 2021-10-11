e314759d5ac7cbf9905cc4d51e8b2f5a307242ca
unknown <dan@sdcsol.com>
2021-10-10 23:26:51 -0400

    made Settings page subscribe to firestore to get realtime updates to the categories colleciton

--- components/CategoryPicker.js
+++ components/CategoryPicker.js
@@ -1,26 +1,46 @@
 import React, {Component} from 'react';
 import SelectMultiple from 'react-native-select-multiple';
 import { View } from 'react-native';
+import { db } from '../firebaseconfig';
 
-const userCategories = ['Donor', 'Volunteer', 'Client','Host'];
+// const userCategories = ['Donor', 'Volunteer', 'Client','Host'];
 
 class CategoryPicker extends Component {
-    state = {selectedCategories: [this.props.selectedCategories]}
+    state = {
+      categories: [],
+      selectedCategoryIds: []
+    }
+    
+    componentDidMount() {
+      db.collection("categories").onSnapshot(catsSnapshot => {
+        const categories = [];
+        catsSnapshot.forEach(doc => {
+          const data = doc.data()
+          categories.push({label: data.name, value: data.id})
+        });
+        this.setState({categories});
+      });
+      // const selectedCategoryIds = [];
+      // catsSnapshot.forEach(doc => selectedCategoryIds.push(doc.data().category_id));
+    }
   
     onSelectionsChange = (selectedCategories) => {
       this.setState({selectedCategories})
     }
   
     render () {
+      const {categories, selectedCategoryIds} = this.state;
+      console.log('categories', categories);
       return (
         <View>
           <SelectMultiple
-          items={userCategories}
-          selectedItems={this.state.selectedCategories}
-          onSelectionsChange={this.onSelectionsChange} />
+            items={categories}
+            selectedItems={selectedCategoryIds}
+            onSelectionsChange={this.onSelectionsChange}
+          />
         </View>
       )
     }
-  }
+}
 // need to be able to save the user's categories here
-  export default CategoryPicker;
+export default CategoryPicker;
