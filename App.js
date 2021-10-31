5d16243835526eadc1b0379e7701dfd664c99684
John Liu <stoic.llama@gmail.com>
2021-10-31 14:59:54 -0400

    Added stivi-notifications details

--- App.js
+++ App.js
@@ -3,14 +3,15 @@ import { StyleSheet } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import { globalContextWrapper } from './contexts/GlobalContext';
+import { globalContextWrapper } from "./contexts/GlobalContext";
 import AdminPage from "./screens/Admin";
 import CalendarPage from "./screens/CalendarPage";
 import SettingsPage from "./screens/SettingsPage";
 import LoginScreen from "./screens/LoginScreen";
+import NotificationsPage from "./screens/NotificationsPage";
 
 const styles = StyleSheet.create({
   container: {
@@ -29,8 +30,8 @@ function AdminStackScreen() {
   return (
     <AdminStack.Navigator
       screenOptions={{
-        headerShown: false
-      }} 
+        headerShown: false,
+      }}
     >
       <AdminStack.Screen name="Login" component={LoginScreen} />
       <AdminStack.Screen name="AdminFunctions" component={AdminPage} />
@@ -41,33 +42,33 @@ function AdminStackScreen() {
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === 'Home') {
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "settings" : "settings-outline";
+            } else if (route.name === "Admin") {
               iconName = focused
-                ? 'home'
-                : 'home-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-              }  
-              else if (route.name === 'Admin') {
-                iconName = focused ? 'notifications-circle' : 'notifications-circle-outline';
-                }
+                ? "notifications-circle"
+                : "notifications-circle-outline";
+            }
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: 'blue',
-          tabBarInactiveTintColor: 'gray',
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "gray",
         })}
       >
+        <Tab.Screen name="Notifications" component={NotificationsPage} />
         <Tab.Screen name="Home" component={CalendarPage} />
         <Tab.Screen name="Settings" component={SettingsPage} />
         <Tab.Screen name="Admin" component={AdminStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
-
   );
 };
 
