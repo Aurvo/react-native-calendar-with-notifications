bdec4209d2d6f323f43665c8b45fcf2f96cd21aa
John Liu <stoic.llama@gmail.com>
2021-11-16 01:15:43 -0500

    Adding Selected Categories Prop Sent to SendMessage Changes

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Button, TextInput, StyleSheet } from "react-native";
 import RegisterExpoToken from "../helpers/RegisterExpoToken";
-import { db, auth } from '../firebaseconfig';
+import { db, auth } from "../firebaseconfig";
 
 const MessageContent = (props) => {
   return (
@@ -13,58 +13,56 @@ const MessageContent = (props) => {
   );
 };
 
-
-const SendMessage = () => {
+const SendMessage = ({ selectedCategories }) => {
   const [messageBody, setMessageBody] = useState("");
-  //gotta get the context for this part instead , const [targetCategories, setTargetCategories] = 
-  
-  //**** 
-  //const expoPushToken = RegisterExpoToken();
-
-  //this should be replace with getting the id of the target categories from category toggler
-  const targetCategories = [1,100,2];
+  const targetCategories = selectedCategories.map(
+    (category) => category.category_id
+  );
   const targetUids = [];
   const targetTokens = [];
   const netTargetTokens = [];
 
   useEffect(() => {
-  const {uid} = db.collection("user_categories").where('category_id','in',targetCategories).onSnapshot(ucatsSnapshot => {
-    
-    ucatsSnapshot.forEach(doc => {
-        const data = doc.data()
-        targetUids.push(data.uid)
-        console.log(data.uid)
-    });
-    const netTargetUids = [];
-    console.log("targetUids:");
-    console.log(targetUids);
-    targetUids.forEach (value => {
-      var findItem = netTargetUids.indexOf(value);
-      if (findItem == -1) {
-        netTargetUids.push(value);
-      }
-    });
-    console.log(netTargetUids);
-    netTargetUids.forEach( targetUid => {
-      db.collection("user_pushId").where('uid','==',targetUid).onSnapshot(nTUSnapshot => {
-      console.log(targetUid)
-      nTUSnapshot.forEach(returnedDoc => {
-          const data = returnedDoc.data()
-          targetTokens.push(data.pushToken)
-          //netTargetTokens.push(data.pushToken)
-          console.log(data.pushToken)
-          var findItem = netTargetTokens.indexOf(data.pushToken);
+    const { uid } = db
+      .collection("user_categories")
+      .where("category_id", "in", targetCategories)
+      .onSnapshot((ucatsSnapshot) => {
+        ucatsSnapshot.forEach((doc) => {
+          const data = doc.data();
+          targetUids.push(data.uid);
+          // console.log(data.uid)
+        });
+        const netTargetUids = [];
+        // console.log("targetUids:");
+        // console.log(targetUids);
+        targetUids.forEach((value) => {
+          var findItem = netTargetUids.indexOf(value);
           if (findItem == -1) {
-            netTargetTokens.push(data.pushToken);
-            console.log(data.pushToken);
+            netTargetUids.push(value);
           }
+        });
+        // console.log(netTargetUids);
+        netTargetUids.forEach((targetUid) => {
+          db.collection("user_pushId")
+            .where("uid", "==", targetUid)
+            .onSnapshot((nTUSnapshot) => {
+              // console.log(targetUid)
+              nTUSnapshot.forEach((returnedDoc) => {
+                const data = returnedDoc.data();
+                targetTokens.push(data.pushToken);
+                //netTargetTokens.push(data.pushToken)
+                // console.log(data.pushToken)
+                var findItem = netTargetTokens.indexOf(data.pushToken);
+                if (findItem == -1) {
+                  netTargetTokens.push(data.pushToken);
+                  // console.log(data.pushToken);
+                }
+              });
+            });
+        });
       });
-    });
-  });
-});
-},[targetCategories]);
+  }, [targetCategories]);
 
-  
   // If you type something in the text box that is a color, the background will change to that
   // color.
   return (
@@ -91,23 +89,22 @@ const SendMessage = () => {
 
   // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
   async function sendPushNotification(netTargetTokens) {
-    netTargetTokens.forEach (targetToken => {
-      const message = {
-        to: targetToken,
-        sound: "default",
-        title: "Message from Charity",
-        body: messageBody,
-        data: { messageBody },
-      }
-      fetch("https://exp.host/--/api/v2/push/send", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Accept-encoding": "gzip, deflate",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(message),
-      });
+    console.log("sending to " + netTargetTokens);
+    const message = {
+      to: netTargetTokens,
+      sound: "default",
+      title: "Message from Charity",
+      body: messageBody,
+      data: { messageBody },
+    };
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
     });
   }
 };
--- screens/AdminPage.js
+++ screens/AdminPage.js
@@ -4,12 +4,24 @@ import SendMessage from "../components/SendMessage";
 import CategoryToggler from "../components/CategoryToggler";
 import EventSection from "./EventSection";
 import HorizontalDivider from "../components/HorizontalDivider";
+import { GlobalContext } from "../contexts/GlobalContext";
 
 const AdminPage = () => {
   // const [isDonorEnabled, setDonor] = React.useState(false);
   // const [isVolunteerEnabled, setVolunteer] = React.useState(false);
   // const [isClientEnabled, setClient] = React.useState(false);
   // const [isHostEnabled, setHost] = React.useState(false);
+  const { categories } = React.useContext(GlobalContext);
+
+  const [selectedCategories, setSelectedCategories] = React.useState(() =>
+    categories.map((category) => ({
+      category_id: category.value,
+      category_name: category.label,
+      isSubscribed: false,
+    }))
+  );
+
+  // categories {label: data.name, value: data.id}
 
   return (
     <View>
@@ -20,7 +32,10 @@ const AdminPage = () => {
           subscription group.
         </Text>
 
-        <CategoryToggler />
+        <CategoryToggler
+          selectedCategories={selectedCategories}
+          onSelectedCategoriesChange={setSelectedCategories}
+        />
 
         {/* <View style={styles.group}>
           <Text style={styles.group__item_text}>Donor</Text>
@@ -65,7 +80,7 @@ const AdminPage = () => {
         <Text style={[styles.group__item_text, { padding: 10 }]}>
           Message To Subscribers
         </Text>
-        <SendMessage />
+        <SendMessage selectedCategories={selectedCategories} />
 
         <HorizontalDivider />
         <EventSection />
